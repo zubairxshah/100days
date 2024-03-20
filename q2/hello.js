083225b50fa84384951cb0dce86ac0aac7bf7b44
zubairxshah <zubairshahmasoomi@gmail.com>
2024-03-20 23:50:55 +0500

    first commit

---
+++ q2/hello.js
@@ -0,0 +1,7 @@
+// Personal Message: Store a person’s name in a variable 
+// and print a message to them, like “Hello name, 
+//would you like to learn some TypeScript today?”
+let name = "David";
+console.log(`Hello ${name}, would you like to learn typescript today.`);
+export {};
+// program ends
---
+++ q3/case.js
@@ -0,0 +1,17 @@
+// Name Cases: Store a person’s name in a variable, and then print 
+// that person’s name in lowercase, uppercase, and titlecase.
+const name = "David Copperfield";
+// printing name in lowercase
+console.log(`Name in lowercase: ${name.toLowerCase()}.`);
+console.log(" ");
+// printing name in the uppercase
+console.log(`Name in UPPERCASE: ${name.toUpperCase()}.`);
+console.log(" ");
+// printing name in Title Case
+const firstName = "john";
+const secondName = "doe";
+console.log("Printing name in Title Case");
+const nameT1 = (firstName.charAt(0).toUpperCase() + firstName.slice(1).toLocaleLowerCase());
+const nameT2 = (secondName.charAt(0).toUpperCase() + secondName.slice(1).toLocaleLowerCase());
+console.log(`${nameT1} ${nameT2}`);
+export {};
